ff8e29b47486026775ed8c4d6e10ba3fa84b67f3
“MahendraDew” <mahendradewangan195@gmail.com>
2023-11-09 22:00:29 +0530

    spotify intitial layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Footer() {
+  return (
+    <Container>Footer</Container>
+  )
+}
+
+const Container = styled.div`
+    background-color: #181818;
+    height: 100%;
+
+`;
