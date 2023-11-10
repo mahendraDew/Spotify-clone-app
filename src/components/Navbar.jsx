3da0ccd262792314d40da8c4488499b440439feb
“MahendraDew” <mahendradewangan195@gmail.com>
2023-11-10 18:40:07 +0530

    navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,76 @@
 import React from 'react'
+import styled from 'styled-components';
+import { FaSearch } from 'react-icons/fa';
+import { CgProfile  } from 'react-icons/cg';
+import { useStateProvider } from '../utils/StateProvider';
 
 export default function Navbar() {
+  const [{ userInfo }] = useStateProvider()
+  console.log({userInfo}, "from navbar.jsx");
   return (
-    <div>Navbar</div>
+    <Container>
+      <div className="search_bar">
+        <FaSearch />
+        <input type="text" placeholder="Artists, Songs, or Podcasts" />
+      </div>
+      <div className="avatar">
+        <a href="#">
+          <CgProfile />
+          <span>{userInfo?.userName}</span>
+        </a>
+      </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+  height: 15vh;
+  position: sticky;
+  top: 0;
+  transition: 0.3s ease-in-out;
+  background-color: none;
+  .search_bar{
+    background-color: white;
+    width: 30%;
+    padding: 0.4rem 1rem;
+    border-radius: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    input{
+      border: none;
+      outline: none;
+      width: 100%;
+      height: 2rem;
+    }
+  }
+  .avatar{
+    background-color: #000;
+    padding: 0.3rem 0.4rem;
+    padding-right: 1rem;
+    border-radius: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0.5rem;
+      text-decoration: none;
+      color: #fff;
+      font-weight: bold;
+      svg{
+        font-size: 1.3rem;
+        background-color: #282828;
+        padding: 0.2rem;
+        border-radius: 1rem;
+        color: #c7c5c5;
+      }
+    }
+  }
+`;
--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 import Body from './Body'
 import Footer from './Footer'
+import { useStateProvider } from '../utils/StateProvider'
+
+import axios from 'axios'
+import { reducerCases } from '../utils/Constants'
 
 export default function Spotify () {
+  const [{ token }, dispatch] = useStateProvider()
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const { data } = await axios.get('https://api.spotify.com/v1/me', {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+      });
+      console.log({data}, "from spotify.jsx");
+      const userInfo = {
+        userId: data.id,
+        userName: data.display_name,
+      };
+      console.log({userInfo}, "from spotify.jsx");
+      dispatch({ type: reducerCases.SET_USER, userInfo });
+      
+    };
+    getUserInfo();
+  }, [dispatch, token]);
+
   return (
     <Container>
       <div className='spotify_body'>
