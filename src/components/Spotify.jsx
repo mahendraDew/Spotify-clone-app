577442c64c06f8da54c864926bd926de1b6f1e10
“MahendraDew” <mahendradewangan195@gmail.com>
2023-11-13 22:39:28 +0530

    updated spotify body

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
@@ -11,6 +11,14 @@ import { reducerCases } from '../utils/Constants'
 
 export default function Spotify () {
   const [{ token }, dispatch] = useStateProvider()
+  const bodyRef = useRef();
+  const [navBackground, setNavBackground] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState(false);
+  const bodyScrolled = () =>{
+    bodyRef.current.scrollTop >= 30 ? setNavBackground(true) : setNavBackground(false);
+    bodyRef.current.scrollTop >= 268 ? setHeaderBackground(true) : setHeaderBackground(false);
+
+  }
 
   useEffect(() => {
     const getUserInfo = async () => {
@@ -20,12 +28,10 @@ export default function Spotify () {
           'Content-Type': 'application/json'
         },
       });
-      console.log({data}, "from spotify.jsx");
       const userInfo = {
         userId: data.id,
         userName: data.display_name,
       };
-      console.log({userInfo}, "from spotify.jsx");
       dispatch({ type: reducerCases.SET_USER, userInfo });
       
     };
@@ -36,10 +42,10 @@ export default function Spotify () {
     <Container>
       <div className='spotify_body'>
         <Sidebar />
-        <div className='body'>
-          <Navbar />
+        <div className='body' ref={bodyRef} onScroll={bodyScrolled}>
+          <Navbar navBackground={navBackground}/>
           <div className='body_contents'>
-            <Body />
+            <Body headerBackground={headerBackground}/>
           </div>
         </div>
       </div>
@@ -68,6 +74,12 @@ const Container = styled.div`
       height: 100%;
       width: 100%;
       overflow: auto;
+      &::-webkit-scrollbar {
+      width: 0.7rem;
+      &-thumb {
+        background-color: rgba(255, 255, 255, 0.6);
+      }
+    }
     }
   }
 `
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -4,6 +4,8 @@ export const initialState = {
     token: null,
     playlists: [],
     userInfo: null,
+    selectedPlaylistId: '6rDVGRVcLzreSiQ3xrCpRK',
+    selectedPlaylist: null,
 };
 
 const reducer = (state, action) => {
@@ -24,6 +26,12 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 userInfo: action.userInfo,
+            };
+        }
+        case reducerCases.SET_PLAYLIST:{
+            return {
+                ...state,
+                selectedPlaylist: action.selectedPlaylist,
             };
         }
         default:
